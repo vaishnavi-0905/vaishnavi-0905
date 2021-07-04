de303864c500d712ee56e0ea118ed5c26695219d
vaishnavi-0905 <84700925+vaishnavi-0905@users.noreply.github.com>
2021-07-04 17:13:11 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const csvParser = require("./api/csvParser")
+router.use('/', express.static('public'))
+router
+    .route("/ContentParser")
+    .post(csvParser)
+    
+
+ 
+module.exports = router;
+© 2021 GitHub, Inc.
